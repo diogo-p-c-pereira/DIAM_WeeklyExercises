405d32fcd728ad05709df243406cb8c63369b751
Diogo Pereira <113448154+diogo-p-c-pereira@users.noreply.github.com>
2025-02-26 19:25:55 +0000

    Class example

--- Week3/aula/src/App.js
+++ Week3/aula/src/App.js
@@ -1,13 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Header/>
+    </div>
+  );
+}
+
+//Contiudo anterior dentro do div
+/*<header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          Funciona!
         </p>
         <a
           className="App-link"
@@ -17,9 +27,6 @@ function App() {
         >
           Learn React
         </a>
-      </header>
-    </div>
-  );
-}
+      </header> */
 
 export default App;
---
+++ Week3/aula/src/Components/Header.js
@@ -0,0 +1,13 @@
+import App from "../App";
+import '../Assets/css/style.css'
+
+function Header() {
+  return(
+    <header>
+      <h1> Os alunos de DIAM são os maiores!!!!!</h1>
+      <h2>Isto veio do Header à parte...</h2>
+    </header>
+  );
+}
+
+export default Header;
