1a37e7df7fba4316943e8caa1de8b032c9fb6400
Diogo Pereira <113448154+diogo-p-c-pereira@users.noreply.github.com>
2025-02-27 00:49:33 +0000

    changed link to self (temporary)

--- Week3/week3/src/MainPage/Header.js
+++ Week3/week3/src/MainPage/Header.js
@@ -3,13 +3,12 @@ function Header() {
   return (
       <header>
           <br/>
-
           <h1>Festival Vilar de Mouros</h1>
           <img src="/images/l_cavilardemouros24.png" width="40%" alt="header image"/>
           <br/> <br/><br/>
           <nav className="menu">
               <ul>
-                  <li><a className="button" href="Main.html">Home</a></li>
+                  <li><a className="button" href=".">Home</a></li>
               </ul>
           </nav>
       </header>
