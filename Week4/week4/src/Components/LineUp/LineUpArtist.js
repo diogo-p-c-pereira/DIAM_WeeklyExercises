1d8ff84c8d1f9a22a75f78d804b5be55089d452a
Diogo Pereira <113448154+diogo-p-c-pereira@users.noreply.github.com>
2025-03-15 22:29:01 +0000

    Week4 Final Submission

--- Week4/week4/src/Components/LineUp/LineUpArtist.js
+++ Week4/week4/src/Components/LineUp/LineUpArtist.js
@@ -6,17 +6,27 @@ function LineUpArtist({ nome, imagem, estilo, descricao, uriVideo, data, hora })
           <h2>{nome}</h2>
           <p>Atuação: {data} às {hora}</p>
           <img src={imagem} alt={nome} onClick={() => showVideo(nome)} width="90%"/>
-          <p>Estilo musical: {estilo}</p>
-          <p>{descricao}</p>
           <iframe id={nome} hidden={true} width="90%" src={uriVideo}
                   title="YouTube video player"
                   referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+          <p>Estilo musical: {estilo}</p>
+          <p>{descricao}</p>
       </div>
   );
 }
 
+
 function showVideo(nome) {
-    document.getElementById(nome).hidden = !document.getElementById(nome).hidden;
+    const iframe = document.getElementById(nome)
+
+    //.getElementById(nome).hidden = !document.getElementById(nome).hidden;
+    iframe.hidden = !iframe.hidden;
+
+    if (!iframe.hidden) {
+        iframe.style.width = '90%';
+        iframe.style.height = '30vh';
+    }
+
 }
 
 export default LineUpArtist;
