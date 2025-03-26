177eeb1f19f372213e38bd00590603f676d1a0ff
Diogo Pereira <113448154+diogo-p-c-pereira@users.noreply.github.com>
2025-03-26 20:14:53 +0000

    CSS for result done

--- week6/src/Components/InquiryResult/Result.js
+++ week6/src/Components/InquiryResult/Result.js
@@ -7,15 +7,16 @@ function Result() {
     return(
         <div className="form-container">
             <div className="inquiry-result">
-                <h1>Resultados do Inquérito</h1>
+                <h2>Resultados do Inquérito</h2>
                 <h3>Artistas Favoritos:</h3>
-                {inquiry.artistasFavoritos.map((artista) => (<div>{artista} <br/></div>))}
+                <h4>{inquiry.artistasFavoritos.map((artista) => (<div>{artista} <br/></div>))}</h4>
                 <br/>
                 <h3>Dia Favorito:</h3>
-                {inquiry.diaPreferido}
+                <h4>{inquiry.diaPreferido}</h4>
                 <br/>
                 <h3>Criticas:</h3>
-                {inquiry.criticas}
+                <h4>{inquiry.criticas}</h4>
+                <br/>
             </div>
         </div>
     );
